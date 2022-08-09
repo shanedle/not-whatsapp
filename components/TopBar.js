94a534c80f2089b490941271c1a45031c9178935
Shane <31248360+shanedle@users.noreply.github.com>
2022-08-09 21:40:45 +0200

    added alert on delete :sparkles:
    added a popup alert when deleting a recipient for better ux

--- components/TopBar.js
+++ components/TopBar.js
@@ -1,13 +1,30 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { useRef } from "react";
 import { deleteDoc, doc } from "firebase/firestore";
-import { Flex, Heading, Avatar, Box, Spacer } from "@chakra-ui/react";
+import {
+  useDisclosure,
+  Flex,
+  Heading,
+  Avatar,
+  Box,
+  Spacer,
+  Button,
+  AlertDialog,
+  AlertDialogBody,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogContent,
+  AlertDialogOverlay,
+} from "@chakra-ui/react";
 import { FaArrowLeft, FaTrashAlt } from "react-icons/fa";
 
 import { db } from "../firebaseconfig";
 
 export default function TopBar({ id, email }) {
   const router = useRouter();
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const cancelRef = useRef();
 
   const handleDelete = async (id) => {
     try {
@@ -28,8 +45,41 @@ export default function TopBar({ id, email }) {
         {email}
       </Heading>
       <Spacer />
-      <Box className="deleteButton" onClick={() => handleDelete(id)}>
-        <FaTrashAlt cursor="pointer" size="1.5em" color="white" />
+      <Box className="deleteButton">
+        <FaTrashAlt
+          cursor="pointer"
+          size="1.5em"
+          color="white"
+          onClick={onOpen}
+        />
+        <AlertDialog
+          isOpen={isOpen}
+          leastDestructiveRef={cancelRef}
+          onClose={onClose}
+        >
+          <AlertDialogOverlay>
+            <AlertDialogContent>
+              <AlertDialogHeader fontSize="lg" fontWeight="bold">
+                Delete Recipient
+              </AlertDialogHeader>
+
+              <AlertDialogBody>Are you sure?</AlertDialogBody>
+
+              <AlertDialogFooter>
+                <Button ref={cancelRef} onClick={onClose}>
+                  Cancel
+                </Button>
+                <Button
+                  colorScheme="red"
+                  ml={3}
+                  onClick={() => handleDelete(id)}
+                >
+                  Delete
+                </Button>
+              </AlertDialogFooter>
+            </AlertDialogContent>
+          </AlertDialogOverlay>
+        </AlertDialog>
       </Box>
     </Flex>
   );
